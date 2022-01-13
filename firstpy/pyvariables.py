78c40d7b4c5a997a493bb2c2c221717ebaf8371b
Aditya Lolla <adityavlolla@gmail.com>
2022-01-12 22:00:38 -0500

    added chapter 4 casting and strings, and started quiz game

---
+++ firstpy/pyvariables.py
@@ -0,0 +1,43 @@
+# chapter 1
+x = 5
+y = "John"
+# print(x)
+# print(y)
+
+# chapter 2
+x = 4
+x = "sally"
+# print(x)
+
+# chapter 3 Casting
+x = str(3)
+print("x value is ", x)
+
+
+#chapter 4 strings
+
+print('hello')
+print("hello")
+
+a = "Hello,World!"
+print(a[1])
+
+#looping through a string
+
+for x in "banana":
+    print (x)
+
+#if statements in strings(useful for quiz)
+
+txt = "The best things in life are free"
+
+if "free" in txt:
+    print("yes, free is present")
+
+txt = "The best things in life are free"
+
+if "Aditya" not in txt:
+    print("No, Aditya is  not present")
+
+
+#
